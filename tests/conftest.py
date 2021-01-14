d9fc0427c291ba851afe553036bdf66eb01942be
TurboKach <incognito@turbokach.me>
2021-01-14 18:28:53 +0300

    flake8 fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,5 @@
 import sys
-from os.path import dirname
-from os.path import abspath
-
+from os.path import abspath, dirname
 
 root_dir = dirname(dirname(abspath(__file__)))
 sys.path.append(root_dir)
